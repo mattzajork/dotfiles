4946ff5dc0abaa313a3136f62c3287448a14c995
Matt Zajork <mzajork@protonmail.com>
2019-02-07 07:40:32 -0600

    remove macos stuff

--- macos/aliases.bash
+++
@@ -1,4 +0,0 @@
-alias l="gls -ahlFN --color --group-directories-first"
-alias ls="gls -FhN --color"
-alias flushdns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias renewdhcp="sudo ipconfig set en0 DHCP"
--- macos/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if test ! $(which brew)
-then
-  echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-exit 0
