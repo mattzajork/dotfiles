c2c4ee5a5c900c1512e6656ad7e66b7146d3b6c5
Matt Zajork <mzajork@protonmail.com>
2019-08-15 16:46:00 -0500

    remove things I no longer use

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,15 +4,6 @@ alias l="ls -ahlF --color=auto"
 alias pubkey="cat $HOME/.ssh/id_rsa.pub"
 alias weather="curl -4 http://wttr.in"
 alias togit="cd $HOME/git"
-alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r' # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade git if -sb breaks
-alias gac='git add -A && git commit -m'
+alias gs='git status -sb'
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
