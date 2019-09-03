47290ef8bc34eb9557127fc0bf97d8611449de9a
Matt Zajork <mzajork@protonmail.com>
2019-09-03 14:50:39 -0500

    update alias and vimrc

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -5,5 +5,6 @@ alias pubkey="cat $HOME/.ssh/id_rsa.pub"
 alias weather="curl -4 http://wttr.in"
 alias togit="cd $HOME/git"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gs='git status -sb'
-alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias pws="keepassxc ~/Documents/database.kdbx &"
+alias sc="import png:- | xclip -selection clipboard -t image/png"
