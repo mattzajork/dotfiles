b2f1598ebdb2c66bd9167eacbeab5030019c7872
Matt Zajork <git@mattz.io>
2023-07-07 16:51:02 -0500

    updates

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -5,6 +5,7 @@ alias pubkey="cat $HOME/.ssh/id_rsa.pub"
 alias weather="curl -4 http://wttr.in"
 alias togit="cd $HOME/git"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias wanip="dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed 's/\"//g'"
+alias wanip="curl ipinfo.io"
 alias pws="keepassxc ~/Documents/database.kdbx &"
 alias sc="import png:- | xclip -selection clipboard -t image/png"
+alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:2.1.1'
