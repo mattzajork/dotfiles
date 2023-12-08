295352135ee074b2c720f6ef66fb31049e2cdac7
Matt Zajork <git@mattz.io>
2023-12-08 16:11:09 -0600

    cleanup

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -3,11 +3,12 @@ alias c="clear"
 alias l="ls -ahlF --color=auto"
 alias pubkey="cat $HOME/.ssh/id_rsa.pub"
 alias weather="curl -4 http://wttr.in"
-alias togit="cd $HOME/git"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias wanip="curl ipinfo.io"
 alias pws="keepassxc ~/Documents/database.kdbx &"
 alias sc="import png:- | xclip -selection clipboard -t image/png"
-alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:2.1.1'
 alias vim='nvim'
 alias vi='nvim'
+# docker stuff
+alias rustscan='sudo docker run -it --rm --name rustscan rustscan/rustscan:2.1.1'
+alias bbot='sudo docker run --network host -v "$HOME/.bbot:/root/.bbot" -v "$HOME/.config/bbot:/root/.config/bbot" -it mzajork/bbot:latest "$@"'
